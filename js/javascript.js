db6c835306bc64ddda626799858616192131de61
AngryHinze <angryhinze@gmail.com>
2023-03-08 16:01:02 +0500

    new-project

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,6 @@
-const burger_icon = document.querySelector(".nav__burger-icon");
-const burger = document.querySelector(".header__menu");
+const burger_icon = document.querySelector("#burger-icon");
+const burger = document.querySelector(".header__burger-container");
 
 burger_icon.onclick = function(){
-    burger.classList.toggle('menu_open');
+    burger.classList.toggle('header__burger-container_open');
 }
